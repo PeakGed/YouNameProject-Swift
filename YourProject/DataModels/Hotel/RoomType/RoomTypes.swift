569ce824ee2a58a39b6dfc0be81c5e84ced7673d
Nattapon <nattapon@ava.fund>
2025-02-27 06:24:41 +0700

    add more func

--- YourProject/DataModels/Hotel/RoomType/RoomTypes.swift
+++ YourProject/DataModels/Hotel/RoomType/RoomTypes.swift
@@ -14,7 +14,38 @@ extension RoomTypes {
 
 // MARK: - Functions
 extension RoomTypes {
-
+    
+    func filter(byID: Int) -> RoomType? {
+        lists.filter({ $0.id == byID }).first
+    }
+    
+    func sorted() -> RoomTypes {
+        let sorted = lists.sorted(by: { $0.name < $1.name })
+        return .init(array: sorted)
+    }
+    
+    func roomType(matchUnitID: Int) -> RoomType? {
+        let filtered = lists.filter({
+            $0.rooms.filter(unitIDs: [matchUnitID]).count > 0
+        })
+        return filtered.first
+    }
+    
+    func filter(unitIDs: Set<Int>) -> RoomTypes {
+        let filtered = lists.filter({
+            let existUnitIDs = $0.rooms.uniqueIDs
+            return unitIDs.intersection(existUnitIDs).count > 0
+        })
+        return .init(array: filtered)
+    }
+    
+    // return unitType contain units.count > 0
+    func filter(status: Room.Status) -> RoomTypes {
+        let filtered = lists.filter({
+            let existUnits = $0.rooms.filter(status: status)
+            return existUnits.count > 0
+        })
+        return .init(array: filtered)
+    }
 }
-
 
--- YourProject/DataModels/Hotel/RoomType/Rooms.swift
+++ YourProject/DataModels/Hotel/RoomType/Rooms.swift
@@ -10,12 +10,36 @@ typealias Rooms = Collection<Room>
 // MARK: Computed properties
 extension Rooms {
 
-
+    var uniqueIDs: Set<Int> {
+        Set(lists.map({ $0.id }))
+    }
+    
+    var codes: [String] {
+        lists.map({ $0.code })
+    }
+    
 }
 
 // MARK: - Functions
 extension Rooms {
 
+    func sorted() -> Rooms {
+        let sorted = lists.sorted(by: { $0.code < $1.code })
+        return .init(array: sorted)
+    }
+    
+    func filter(unitIDs: Set<Int>) -> Rooms {
+        let filtered = lists.filter({ unitIDs.contains($0.id) })
+        return .init(array: filtered)
+    }
+    
+    func filter(status: Room.Status) -> Rooms {
+        let filtered = lists.filter({ $0.status == status })
+        return .init(array: filtered)
+    }
+    
+    func unit(matchID: Int) -> Room? {
+        return lists.first(where: { $0.id == matchID })
+    }
 
 }
-
