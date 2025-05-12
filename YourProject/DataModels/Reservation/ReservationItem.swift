c95026f8ded73347cee3a3c4fd769d7d4234d90c
Nattapon <nattapon@ava.fund>
2025-05-12 20:03:12 +0700

    new files

---
+++ YourProject/DataModels/Reservation/ReservationItem.swift
@@ -0,0 +1,12 @@
+//
+//  ReservationItem.swift
+//  YourProject
+//
+//  Created by IntrodexMini on 12/5/2568 BE.
+//
+
+import Foundation
+
+struct ReservationItem: Codable {
+    
+}
---
+++ YourProject/DataModels/Reservation/ReservationItems.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationItems.swift
+//  YourProject
+//
+//  Created by IntrodexMini on 12/5/2568 BE.
+//
+
+typealias ReservationItems = Collection<ReservationItem>
+
+// MARK: Computed properties
+extension ReservationItems {
+    
+}
+
+// MARK: - Functions
+extension ReservationItems {
+    
+}
