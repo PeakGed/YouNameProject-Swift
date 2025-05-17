0728cf6a9edf23ab58291db076b1ee0b39f4eae3
Nattapon <nattapon@ava.fund>
2025-05-17 20:04:09 +0700

    add new model

---
+++ YourProjectTests/DataModelTests/Additional/FinancialRecord.swift
@@ -0,0 +1,168 @@
+//
+//  FinancialRecord.swift
+//  YourProject
+//
+//  Created by IntrodexMini on 17/5/2568 BE.
+//
+
+import Foundation
+
+struct FinancialRecord: Codable {
+    let id: Int
+    let name: String
+    let paymentMethod: String
+    let note: String?
+    let timestamp: Date
+    let amount: Double
+    let creator: Creator
+    let recordableId: Int
+    let recordableType: String
+    let createdAt: Date
+    let updatedAt: Date
+    let hotelId: Int
+    let bankAccount: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case paymentMethod = "payment_method"
+        case note
+        case timestamp
+        case amount
+        case creator
+        case recordableId = "recordable_id"
+        case recordableType = "recordable_type"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+        case hotelId = "hotel_id"
+        case bankAccount = "bank_account"
+    }
+    
+    init(id: Int,
+         name: String,
+         paymentMethod: String,
+         note: String?,
+         timestamp: Date,
+         amount: Double,
+         creator: Creator,
+         recordableId: Int,
+         recordableType: String,
+         createdAt: Date,
+         updatedAt: Date,
+         hotelId: Int,
+         bankAccount: String?) {
+        self.id = id
+        self.name = name
+        self.paymentMethod = paymentMethod
+        self.note = note
+        self.timestamp = timestamp
+        self.amount = amount
+        self.creator = creator
+        self.recordableId = recordableId
+        self.recordableType = recordableType
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.hotelId = hotelId
+        self.bankAccount = bankAccount
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        paymentMethod = try container.decode(String.self, forKey: .paymentMethod)
+        note = try container.decodeIfPresent(String.self, forKey: .note)
+        timestamp = try container.decode(String.self, forKey: .timestamp).tryToDate(dateFormat: FormConfig.DateFormat.datetimeISO)
+        amount = try container.decode(String.self, forKey: .amount).trytoDouble()
+        creator = try container.decode(Creator.self, forKey: .creator)
+        recordableId = try container.decode(Int.self, forKey: .recordableId)
+        recordableType = try container.decode(String.self, forKey: .recordableType)
+        hotelId = try container.decode(Int.self, forKey: .hotelId)
+        bankAccount = try container.decodeIfPresent(String.self, forKey: .bankAccount)
+        createdAt = try container.decode(String.self, forKey: .createdAt).tryToDate(dateFormat: FormConfig.DateFormat.datetimeISO)
+        updatedAt = try container.decode(String.self, forKey: .updatedAt).tryToDate(dateFormat: FormConfig.DateFormat.datetimeISO)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(paymentMethod, forKey: .paymentMethod)
+        try container.encode(note, forKey: .note)
+        try container.encode(timestamp.toDateString(FormConfig.DateFormat.datetimeISO), forKey: .timestamp)
+        try container.encode(String(amount), forKey: .amount)
+        try container.encode(creator, forKey: .creator)
+        try container.encode(recordableId, forKey: .recordableId)
+        try container.encode(recordableType, forKey: .recordableType)
+        try container.encode(hotelId, forKey: .hotelId)
+        try container.encode(bankAccount, forKey: .bankAccount)
+        try container.encode(createdAt.toDateString(FormConfig.DateFormat.datetimeISO), forKey: .createdAt)
+        try container.encode(updatedAt.toDateString(FormConfig.DateFormat.datetimeISO), forKey: .updatedAt)
+    }
+}
+
+extension FinancialRecord {
+    struct Creator: Codable {
+        let id: Int
+        let email: String
+        let firstName: String
+        let lastName: String
+        //let logoImage: String?
+        let role: String
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+            id = try container.decode(Int.self, forKey: .id)
+            email = (try? container.decode(String.self, forKey: .email)) ?? ""
+            firstName = (try? container.decode(String.self, forKey: .firstName)) ?? ""
+            lastName = (try? container.decode(String.self, forKey: .lastName)) ?? ""
+            role = (try? container.decode(String.self, forKey: .role)) ?? ""
+        }
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            
+            try container.encode(id, forKey: .id)
+            try container.encode(email, forKey: .email)
+            try container.encode(firstName, forKey: .firstName)
+            try container.encode(lastName, forKey: .lastName)
+            try container.encode(role, forKey: .role)
+        }
+        
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case email
+            case firstName = "first_name"
+            case lastName = "last_name"
+            //case logoImage = "logo_image"
+            case role
+        }
+    }
+}
+
+/*
+ {
+            "id": 440,
+            "name": "PAYMENT",
+            "payment_method": "Bank Transfer",
+            "note": null,
+            "timestamp": "2024-04-21T13:33:54.748+07:00",
+            "amount": 2111,
+            "creator": {
+                "id": 38,
+                "email": "test1@email.com",
+                "first_name": "John2",
+                "last_name": "Doe2",
+                "logo_image": null,
+                "role": "owner"
+            },
+            "recordable_id": 1067,
+            "recordable_type": "Reservation",
+            "created_at": "2024-04-21T13:33:54.756+07:00",
+            "updated_at": "2024-04-21T13:33:54.756+07:00",
+            "hotel_id": 105,
+            "bank_account": null
+        }
+ */
