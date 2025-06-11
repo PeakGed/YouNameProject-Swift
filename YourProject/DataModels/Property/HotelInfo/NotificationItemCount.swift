51ebf06b10dee831ce6816cd58cfe151ecdd491d
nattapon <>
2025-06-11 14:06:40 +0700

    init model

---
+++ YourProject/DataModels/Property/HotelInfo/NotificationItemCount.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationItemCount.swift
+//  YourProject
+//
+//  Created by IntrodexMac on 11/6/2568 BE.
+//
+
+/*
+ {
+     "reservations": {
+         "unread": 0,
+         "readed": 2,
+         "total": 2
+     },
+     "cm_bookings": {
+         "unread": 1,
+         "readed": 2,
+         "total": 3
+     }
+ }
+ */
