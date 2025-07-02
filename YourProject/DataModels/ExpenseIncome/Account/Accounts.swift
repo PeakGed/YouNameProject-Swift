f28cef2f3f21adf1ae3d63954d5f3a5a933ef1be
Nattapon <nattapon@ava.fund>
2025-07-02 13:46:29 +0700

    add new model

--- YourProject/DataModels/ExpenseIncome/Account/Accounts.swift
+++ YourProject/DataModels/ExpenseIncome/Account/Accounts.swift
@@ -5,3 +5,14 @@
 //  Created by IntrodexMini on 11/5/2568 BE.
 //
 
+typealias Accounts = Collection<Account>
+
+// MARK: Computed properties
+extension Accounts {
+    
+}
+
+// MARK: - Functions
+extension Accounts {
+    
+}
--- YourProject/DataModels/ExpenseIncome/Payee/Payees.swift
+++ YourProject/DataModels/ExpenseIncome/Payee/Payees.swift
@@ -5,3 +5,14 @@
 //  Created by IntrodexMini on 11/5/2568 BE.
 //
 
+typealias Payees = Collection<Payee>
+
+// MARK: Computed properties
+extension Payees {
+    
+}
+
+// MARK: - Functions
+extension Payees {
+    
+}
