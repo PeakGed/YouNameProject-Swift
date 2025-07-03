1177a9f8b2c057f749b0a6c4da18c2e041555bc2
Nattapon <nattapon@ava.fund>
2025-07-03 13:56:06 +0700

    feat: new model

--- YourProject/DataModels/Reservation/ReservationLog/ReservationLogs.swift
+++ YourProject/DataModels/Reservation/ReservationLog/ReservationLogs.swift
@@ -5,3 +5,14 @@
 //  Created by IntrodexMini on 23/5/2568 BE.
 //
 
+typealias ReservationLogs = Collection<ReservationLog>
+
+// MARK: Computed properties
+extension ReservationLogs {
+    
+}
+
+// MARK: - Functions
+extension ReservationLogs {
+    
+}
